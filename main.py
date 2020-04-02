6e1e877cd224223cb4c30766ffd4107f0936560f
zhouyang <1757561862@qq.com>
2020-04-02 17:08:35 +0800

    Add image_utils

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import argparse
+import base64
 
 import torch
+import cv2
+import numpy as np
 from flask import Flask, request, jsonify
 
+from utils import image_utils
 import global_var
 
 app = Flask(__name__)
@@ -13,7 +17,7 @@ def parse_arg():
     parser.add_argument('--ip', type=str, default='192.168.1.6', help='host ip')
     parser.add_argument('--port', type=str, default=6666, help='host port')
     parser.add_argument('--is_use_gpu', type=bool, default=False, help='use gpu or cpu')
-    parser.add_argument('--model_path', type=str, default='checkpoint/DSTN_448.pt',
+    parser.add_argument('--model_path', type=str, default='checkpoint/efficientdet(2).pt',
                         help='model path for deploying')
     return parser.parse_args()
 
@@ -23,17 +27,43 @@ def load_model(model_path, is_use_gpu):
     :param model_path:
     :return:
     """
-    model = torch.jit.load(model_path)
+    print(model_path)
+    model = torch.jit.load(model_path, map_location='cpu')
     model.eval()
     model.cuda() if is_use_gpu else model.cpu()
     return model
     # print(model)
 
 
-@app.route("/predict", methods=['POST','GET'])
+@app.route("/predict", methods=['POST', 'GET'])
 def predict():
     # Initialize the data dictionary that will be returned from the view.
     data = {"success": False}
+
+    # img base64
+    # base64_str = image_utils.img_base64('images/shape_test/36002.jpg')
+    # new_image = image_utils.process_image(base64_str)
+
+    img = cv2.imread('images/shape_test/36003.jpg')
+    height, width, _ = img.shape
+    scale = 0
+    if height > width:
+        scale = height / 512
+    else:
+        scale = width / 512
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32) / 255.
+    img = cv2.resize(img, (512, 512))
+    new_image = np.zeros((512, 512, 3))
+    new_image[0:512, 0:512] = img
+    new_image = torch.from_numpy(new_image)
+    new_image = new_image.permute(2, 0, 1).float().unsqueeze(dim=0)
+
+    with torch.no_grad():
+        boxes = global_var.model(new_image)
+    print(boxes.shape[0])
+    if boxes.shape[0] > 0:
+        print(boxes.shape[0])
     return data
 
 def main():
---
+++ utils/image_utils.py
@@ -0,0 +1,69 @@
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
+import numpy as np
+import torch
+
+import main
+import global_var
+
+
+# 图片文件打开为base64
+def img_base64(img_path):
+    with open(img_path, "rb") as f:
+        base64_str = base64.b64encode(f.read())
+        return base64_str
+
+
+# PIL转cv2
+def pil_cv2(img_path):
+    image = Image.open(img_path)
+    img = cv2.cvtColor(np.asarray(image),cv2.COLOR_RGB2BGR)
+    return img
+
+
+# base64转PIL
+def base64_pil(base64_str):
+    image = base64.b64decode(base64_str)
+    image = BytesIO(image)
+    image = Image.open(image)
+    return image
+
+"""
+base64 和 cv2
+"""
+def base64_cv2(base64_str):
+    imgString = base64.b64decode(base64_str)
+    nparr = np.frombuffer(imgString,np.uint8)
+    image = cv2.imdecode(nparr,cv2.IMREAD_COLOR)
+    return image
+
+def process_image(img_base64_str):
+    img = base64_cv2(img_base64_str)
+    height, width, _ = img.shape
+    scale = 0
+    if height > width:
+        scale = height / 512
+    else:
+        scale = width / 512
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32) / 255.
+    img = cv2.resize(img, (512, 512))
+    new_image = np.zeros((512, 512, 3))
+    new_image[0:512, 0:512] = img
+    new_image = torch.from_numpy(new_image)
+    new_image = new_image.permute(2, 0, 1).float().unsqueeze(dim=0)
+    return new_image
+
+
+if __name__ == "__main__":
+    global_var.model = main.load_model("../checkpoint/efficientdet(2).pt", False)
+    img_base64_str = img_base64('../images/shape_test/36001.jpg')
+    img = process_image(img_base64_str)
+    with torch.no_grad():
+        boxes = global_var.model(img)
+    print(boxes.size())
+
+
+
